b5955c2ea92133bbc5359e7d120ad314dc9b8dcd
Lee Jones KE <93858479+Leejoneske@users.noreply.github.com>
2026-05-15 08:45:49 +0300

    Update import for defineConfig in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "@tanstack/start/config";
+import { defineConfig } from "@tanstack/react-start/plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
