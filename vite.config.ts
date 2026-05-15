3d76d1a1d916371657d2286f064a44be21b1908c
Lee Jones KE <93858479+Leejoneske@users.noreply.github.com>
2026-05-15 08:56:58 +0300

    Simplify tanstackStart plugin configuration

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
-    tanstackStart({
-      server: { preset: "vercel" },
-    }),
+    tanstackStart(),
     TanStackRouterVite({ autoCodeSplitting: true }),
     tailwindcss(),
     tsconfigPaths(),
